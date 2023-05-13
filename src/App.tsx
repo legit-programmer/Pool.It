70b9f275de790e62af6735037296cb0cb28cf5d0
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-05-14 00:08:13 +0530

    deploy changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
-import Cart from "./components/Cart";
 
 const products = [
   {
